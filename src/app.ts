342f2d40f9c93742be67fa5e2d1b77e65d5305f5
Noah Manaigre <nmanaigre2@academic.rrc.ca>
2025-11-05 20:54:42 -0600

    Added headers screenshot

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import dotenv from "dotenv";
 import { getHelmetConfig } from "../config/helmetConfig";
 import employeeRoutes from "./api/v1/routes/employeeRoutes";
 import branchRoutes from "./api/v1/routes/branchRoutes";
+import cors from "cors";
+import getCorsOptions from "../config/corsConfig";
+
 
 dotenv.config();
 
@@ -19,6 +22,8 @@ app.use(morgan("combined"));
 // Routes
 app.use("/api/v1/employees", employeeRoutes);
 app.use("/api/v1/branches", branchRoutes);
+
+app.use(cors(getCorsOptions()));
 
 // Health check endpoint
 app.get("/health", (req: Request, res: Response) => {
